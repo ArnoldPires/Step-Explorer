437b90ccb31f207c61f306bfd28b2d18b928f355
ArnoldPires <arnold.pires92@gmail.com>
2023-10-09 13:45:41 -0400

    Created a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
-import Header from './components/header/header'
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <Router>
       <>
         <Header />
+        <Footer />
       </>
     </Router>
   )
