8c8c95ea623e5a309761aeed1aba9c4ea6703729
ArnoldPires <arnold.pires92@gmail.com>
2023-10-09 13:36:44 -0400

    Created a header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import "./header.css";
+
+const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+  return (
+     <header className="header">
+      <div className="logo">
+        <Link to="/"><img src="/images/cover.png" alt="Step Explorer Logo" /></Link>
+      </div>
+      <nav className={`nav ${mobileMenuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><Link to="#">Hiking Guide</Link></li>
+          <li><Link to="#">Login</Link></li>
+          <li><Link to="#">Sign up</Link></li>
+        </ul>
+      </nav>
+      <div className={`hamburger-menu ${mobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
