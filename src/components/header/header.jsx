e13c19e9f269539f71481de2e39cff1cac1cb7b7
ArnoldPires <arnold.pires92@gmail.com>
2023-10-23 01:56:00 -0400

    Created a, create a hike page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <ul>
           <li><Link to="/HikeGuide">Hiking Guide</Link></li>
           <li><Link to="/HikeSearch">Find A Hike/Trail</Link></li>
-          <li><Link to="/NewTrails">Create A Hike</Link></li>
+          <li><Link to="/CreateTrail">Create A Hike</Link></li>
           <li><Link to="/Login">Login</Link></li>
           <li><Link to="/Signup">Sign up</Link></li>
           <li><Link to="/test">test</Link></li>
